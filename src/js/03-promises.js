3dacf73bc9fdc270102686d4335fd2281e442faf
IlyaGubinn <ilya.gubin29@gmail.com>
2023-02-05 13:48:37 +0300

    выполняет 3 задание

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+refs = {
+formRef: document.querySelector('.form'),
+delayRef: document.querySelector('[name="delay"]'),
+stepRef: document.querySelector('[name="step"]'),
+amountRef: document.querySelector('[name="amount"]'),
+}
+
+
+
+refs.formRef.addEventListener('submit', runCreatePromises)
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+ const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position,delay});
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+function runCreatePromises(event) {
+  event.preventDefault();
+
+
+
+let delay = +(refs.delayRef.value);
+const amount = +(refs.amountRef.value);
+const step = +(refs.stepRef.value);
+
+
+for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;
   }
+  fornRef.reset()
 }
